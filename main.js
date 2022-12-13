d0003e05783a9a79b8a50be73ecbc980c77bbafd
Liam Bowie <liambowie16@gmail.com>
2022-12-13 14:48:34 +0000

    Add a method to the Field class that moves the player based on an argument

--- main.js
+++ main.js
@@ -4,14 +4,37 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+let alive = true;
 
 class Field { 
-    constructor(field){ 
+    constructor(field, positionX, positionY){ 
         this.field = field;
+        this.positionX = 0;
+        this.positionY = 0;
+    }
+
+    movePlayer(direction) { 
+        switch(direction) { 
+            case 'w': 
+                this.positionX--;
+                break;
+            case 's': 
+                this.positionX++;
+                break;
+            case 'a': 
+                this.positionY--;
+                break;
+            case 'd': 
+                this.positionY++;
+            default: 
+             console.log("Enter a valid direction: w (up), a (left), s (down), d (right)")
+        }
+
+        this.field[this.positionX][this.positionY] = pathCharacter;
     }
 
     print() { 
-        let string = '';
+        let string = '\n';
         for (let i = 0; i < this.field.length; i++) {
             for (let j = 0; j < this.field.length; j++) {
                 string += this.field[i][j];
@@ -24,18 +47,20 @@ class Field {
     }
 }
 
-// GAME LOOP
-
-const myField = new Field([
+const gameField = new Field([
     ['*', '░', 'O'],
     ['░', 'O', '░'],
     ['░', '^', '░'],
   ]);
 
-const alive = true;
+  gameField.print();
+  gameField.movePlayer('h');
+  gameField.print();
+  
+// console.log(`Use WASD keys to move your character ${pathCharacter} around the map to find your hat (${hat})`);
+// console.log(`Don't fall in the holes (${hole}), and don't walk off the edge!`);
 
-console.log(`Use WASD keys to move your character ${pathCharacter} around the map and find your hat (${hat}). Don't fall in the holes (${hole}), and don't walk off the edge!`);
-while(alive) { 
-    myField.print();
-    const move = prompt("Which way?: ");
-}
+// while(alive) { 
+//     myField.print();
+//     const move = prompt("Which way?: ");
+// }

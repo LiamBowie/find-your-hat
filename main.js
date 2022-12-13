d9c4d72d6baa000866ba1889aac8bbac29400d05
Liam Bowie <liambowie16@gmail.com>
2022-12-13 15:20:42 +0000

    Added method that evaluates whether the current position of a player is valid or ends the game

--- main.js
+++ main.js
@@ -4,39 +4,67 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
-let alive = true;
 
 class Field { 
-    constructor(field, positionX, positionY){ 
+    constructor(field){ 
         this.field = field;
-        this.positionX = 0;
-        this.positionY = 0;
+        this.playerX = 0;
+        this.playerY = 0;
     }
 
+    // Updates the players current (x, y) co-ordinates. 
     movePlayer(direction) { 
         switch(direction) { 
             case 'w': 
-                this.positionX--;
+                this.playerX--;
                 break;
             case 's': 
-                this.positionX++;
+                this.playerX++;
                 break;
             case 'a': 
-                this.positionY--;
+                this.playerY--;
                 break;
             case 'd': 
-                this.positionY++;
+                this.playerY++;
+                break;
             default: 
              console.log("Enter a valid direction: w (up), a (left), s (down), d (right)")
         }
 
-        this.field[this.positionX][this.positionY] = pathCharacter;
+        if(this.evaluateCurrentPosition(this.playerX, this.playerY)){ 
+
+            this.field[this.playerX][this.playerY] = pathCharacter;
+        }
     }
 
+    // Returns false if the current position results in the end of a game. 
+    evaluateCurrentPosition(x, y) { 
+ 
+        if ( x < 0 || x >= this.field.length || y < 0 || y >= this.field[x]) { 
+
+            console.log("You have fell off the map!");
+            return false;
+        }
+        else if ( this.field[x][y] === hole ){ 
+            
+            console.log("You have fell into a hole!");
+            return false;
+        }
+        else if ( this.field[x][y] === hat) {
+
+            console.log(`CONGRATULATIONS! You found your hat ${hat}` )
+            return false;
+        }
+        else { 
+            return true;
+        }
+    }
+
+    // Prints the current state of the game to the console. 
     print() { 
         let string = '\n';
         for (let i = 0; i < this.field.length; i++) {
-            for (let j = 0; j < this.field.length; j++) {
+            for (let j = 0; j < this.field[i].length; j++) {
                 string += this.field[i][j];
                 
             }
@@ -50,13 +78,20 @@ class Field {
 const gameField = new Field([
     ['*', '░', 'O'],
     ['░', 'O', '░'],
+    ['░', 'O', '░'],
     ['░', '^', '░'],
   ]);
 
-  gameField.print();
-  gameField.movePlayer('h');
-  gameField.print();
-  
+gameField.print();
+gameField.movePlayer('s');
+gameField.print();
+gameField.movePlayer('s');
+gameField.print();
+gameField.movePlayer('s');
+gameField.print();
+gameField.movePlayer('d');
+
+
 // console.log(`Use WASD keys to move your character ${pathCharacter} around the map to find your hat (${hat})`);
 // console.log(`Don't fall in the holes (${hole}), and don't walk off the edge!`);
 

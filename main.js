2334c322d535e7de9f0f831cf0ff5f024a3158c0
Liam Bowie <liambowie16@gmail.com>
2022-12-13 18:38:33 +0000

    The player can now start at a random location

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Game { 
-    constructor(field){ 
+    constructor(field, playerX=0, playerY=0){ 
         this.field = field;
-        this.playerX = 0;
-        this.playerY = 0;
+        this.playerX = playerX;
+        this.playerY = playerY;
         this.playing = true;
     }
 
@@ -90,13 +90,24 @@ class Game {
         }
     }
 
+    // Returns an object that contains the x and y co-ordinates of a player character
+    static findPlayer(field) { 
+        for(let i = 0; i < field.length; i++) { 
+            for(let j = 0; j < field[i].length; j++) { 
+                if (field[i][j] === pathCharacter) { 
+                    return {x: i, y: j}
+                }
+            }
+        }
+    }
+
     // Generates a random field with a specified height and width.
-    static generateField(height, width, percentage = 25) { 
+    static generateField(height, width, randomStartPosition = false, percentage = 25) { 
         let field = [];
         for(let i = 0; i < height; i++) { 
             field[i] = [];
             for(let j = 0; j < width; j++) { 
-
+                
                 if ( Math.floor(Math.random() * 100) < percentage) { 
                     field[i][j] = hole;
                 }
@@ -105,15 +116,36 @@ class Game {
                 }
             }
         }
+        let playerX = 0;
+        let playerY = 0;
 
-        field[0][0] = pathCharacter;
-        field[Math.floor(Math.random()*height)][Math.floor(Math.random()*width)] = hat;
+        if (randomStartPosition) { 
+            playerX = Math.floor(Math.random()*height);
+            playerY = Math.floor(Math.random()*width);
+        }
 
+        field[playerX][playerY] = pathCharacter;
+
+        let hatPositionX = Math.floor(Math.random()*height);
+        let hatPositionY = Math.floor(Math.random()*width);
+
+        while (this.playerX === hatPositionX && this.playerY === hatPositionY){ 
+            hatPositionX = Math.floor(Math.random()*height);
+            hatPositionY = Math.floor(Math.random()*width);
+        }
+        field[hatPositionX][hatPositionY] = hat;
         return field;
     }
 }
 
-const fyh = new Game(Game.generateField(7, 15));
+// Player starts in the top left corner. 
+defaultField = Game.generateField(12, 12);
+
+// Player starts in a random location. 
+randomStartField = Game.generateField(14, 28, true);
+playerCoords = Game.findPlayer(randomStartField);
+
+const fyh = new Game(randomStartField, playerCoords.x, playerCoords.y);
 fyh.play();
 
 // const findYourHat = new Game([
